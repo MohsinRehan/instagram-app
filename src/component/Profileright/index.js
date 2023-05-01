835af3635410d068ce499813f5084d14c825e29a
MohsinRehan <mohsindev53@gmail.com>
2023-05-01 12:24:17 +0500

    rightprofile setting

--- src/component/Profileright/index.js
+++ src/component/Profileright/index.js
@@ -3,25 +3,28 @@ import React from "react";
 const Profile = () => {
   return (
     <>
-      <div className="flex justify-between mt-5 w-[350px] p-5">
-        <div className="flex">
-          <div>
+      <div className=" mt-5 w-[350px] p-5">
+        <div className="flex justify-between">
+          <div className="flex">
             <img
               src="../Assets/image/mohsin.JPG"
               className="w-14 h-14 rounded-full"
             />
-          </div>
-          <div>
+             <div>
             <p className="ml-2 text-[14px] font-semibold">its_MohsinRehan</p>
             <p className="ml-3 text-[14px]">Mohsin Ali</p>
           </div>
+          </div>
+         
+          <div>
+            <button className="text-[12px] text-[#0095f6] mt-5">Switch</button>
+          </div>
         </div>
+
         <div>
-          <button className="text-[12px] text-[#0095f6] mt-5">Switch</button>
+          <p>Suggested for you</p>
+          <button>See All</button>
         </div>
-      </div>
-      <div>
-        <p>Suggested for you</p>
       </div>
     </>
   );
